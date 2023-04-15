ad8815591b72e84c914ef152296ce87e4028e2c8
wangdashuai <fanhua1323@163.com>
2023-04-15 22:38:20 +0800

    feat(项目初始化): 🎉 开天辟地第一次

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+// import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import UnoCss from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [NaiveUiResolver()]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    }),
+    // createSvgIconsPlugin({
+    //   iconDirs: [fileURLToPath(new URL('./src/assets/icons', import.meta.url))],
+    //   symbolId: 'icon-[dir]-[name]',
+    //   inject: 'body-last'
+    // }),
+    UnoCss()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
