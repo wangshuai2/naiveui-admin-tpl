452430bdbcd6718f50ac0cb00fb661cebcb5bcb3
wangshuai <399397446@qq.com>
2023-04-17 16:49:28 +0800

    feat(功能开发): ✨ 调整根节点配置，添加loading-bar

--- src/components/Application/Application.vue
+++ src/components/Application/Application.vue
@@ -1,13 +1,36 @@
 <template>
-  <n-dialog-provider>
-    <n-notification-provider>
-      <n-message-provider>
-        <slot />
-      </n-message-provider>
-    </n-notification-provider>
-  </n-dialog-provider>
+  <n-loading-bar-provider>
+    <n-dialog-provider>
+      <n-notification-provider>
+        <n-message-provider>
+          <slot></slot>
+          <NaiveProviderContent />
+        </n-message-provider>
+      </n-notification-provider>
+    </n-dialog-provider>
+  </n-loading-bar-provider>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDialog, useLoadingBar, useMessage, useNotification } from 'naive-ui'
+import { defineComponent, h } from 'vue'
+
+function registerNaiveTools() {
+  window.$loadingBar = useLoadingBar()
+  window.$dialog = useDialog()
+  window.$message = useMessage()
+  window.$notification = useNotification()
+}
+
+const NaiveProviderContent = defineComponent({
+  name: 'NaiveProviderContent',
+  setup() {
+    registerNaiveTools()
+  },
+  render() {
+    return h('div')
+  }
+})
+</script>
 
 <style scoped></style>
