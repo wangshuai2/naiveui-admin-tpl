f3482e0c2cd6ad9a7c2ef954ca97624aa7fa90e2
wangshuai <399397446@qq.com>
2023-04-17 16:50:40 +0800

    feat(功能开发): ✨ 调整types配置

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const Component: DefineComponent<{}, {}, any>
+  export default Component
+}
+
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
